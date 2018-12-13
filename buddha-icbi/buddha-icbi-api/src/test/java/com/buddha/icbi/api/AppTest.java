bd9d19d196cfef1f6b69ebf0f5468297651de9c1
javawww <1003632308@qq.com>
2018-12-13 18:58:45 +0800

    哈哈哈哈哈

--- buddha-icbi/buddha-icbi-api/src/test/java/com/buddha/icbi/api/AppTest.java
+++ buddha-icbi/buddha-icbi-api/src/test/java/com/buddha/icbi/api/AppTest.java
@@ -8,11 +8,6 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName name of the test case
-	 */
 	public AppTest(String testName) {
 		super(testName);
 	}
